5f0bf3516168c4a11704dde56015c381a75bd664
Jit Das <jitdas662@gmail.com>
2026-07-07 01:54:18 +0530

    Day 1:Setup  React Project and learned JSX

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import MyComponent from "./mycomponent";
+
+function App(){
+  return(
+    <div>
+    <h1>Hi, i'm Jit😀</h1>
+    <h2>day 1 of react</h2>
+    <p>i will become a fullstack debeloper!</p>
+    <MyComponent/>
+    <ul>
+      <li>home</li>
+      <li>contact</li>
+      <li>profile</li>
+    </ul>
+    </div>
+  );
+  }
+export default App;
---
+++ src/mycomponent.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const MyComponent = () =>{
+    return <h1>Hello React</h1>;
+
+};
+export default MyComponent;
