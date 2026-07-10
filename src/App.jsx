a5fe300c21f661e33926926774c0b092922945c1
Jit Das <jitdas662@gmail.com>
2026-07-11 01:13:50 +0530

    Dat 4 React: Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Hero from "./Hero";
 import About from "./About";
 import Footer from "./Footer";
 import Card from "./Card";
+import Student from "./Student";
 function App(){
   return(
     <>
-    <Card name="Jit Das" role="future full stack developer" age="22" college="brainware university" city="Barasat"/>
-    <Card name="Aman" role="frontend developer" age="23" college="Adamas University" city="kolkata"/>
-    <Card name="moupriya" role="backend developer" age="22" college="brainware university" city="Nandigram"/>
+    <Student name="Jit Das" role="future full stack developer" age={22} college="brainware university" city="Barasat"/>
+    <Student name="Aman" role="frontend developer" age={23} college="Adamas University" city="kolkata"/>
+    <Student name="moupriya" role="backend developer" age={22} college="brainware university" city="Nandigram"/>
+    <Student name="Shrabanti Das" role="makeup  artist" age={32} college="Barasat College" city="Barasat"/>
+  
   
 
     </>
---
+++ src/Student.jsx
@@ -0,0 +1,11 @@
+function Student({name,age,city,college}){
+    return(
+        <div>
+            <h2>Name: {name}</h2>
+            <p>Age: {age}</p>
+            <p>City: {city}</p>
+            <p>College: {college}</p>
+        </div>
+    );
+}
+export default Student;
